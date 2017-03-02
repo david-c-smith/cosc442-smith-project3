2991b1c0c7800cb3e58dd8e58de1a31efbeff8a3
david-c-smith <dcsmith0515@gmail.com>
2017-03-01 19:49:23 -0500

    Added rest of the junit test files from project 2 into this repo

---
+++ VendingMachine2/src/edu/towson/cis/cosc442/project3/vendingmachine/VendingMachineItemTest.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package edu.towson.cis.cosc442.project3.vendingmachine;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Dave
+ *
+ */
+public class VendingMachineItemTest {
+	
+	private VendingMachineItem item;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		item = new VendingMachineItem("Test item", 10);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		System.gc();
+	}
+
+	
+	/**
+	 * Testing the constructor for name and price values 
+	 */
+	@Test
+	public void constructorSetValuesTest () {
+		assertEquals("Test item", item.getName());
+		assertEquals(10, item.getPrice(), 0.001);
+	}
+	
+	/**
+	 * Testing the exception for a price < 0 in the constructor
+	 */
+	@Test (expected = VendingMachineException.class)
+	public void VendingMachineItemLessThanZeroTest () {
+		item = new VendingMachineItem("Test item", -5);
+	}
+	
+	/**
+	 * Testing the getName method for values
+	 */
+	@Test
+	public void getNameTest() {
+		assertEquals("Test item", item.getName());
+	}
+	
+	/**
+	 * Testing the getPrice method for values 
+	 */
+	@Test
+	public void getPriceTest() {
+		assertEquals(10, item.getPrice(), 0.001);
+	}
+}
